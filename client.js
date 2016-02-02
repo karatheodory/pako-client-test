a675ea4e34ad0d3e144131e6ff85444f0c0bf650
Vasily Loginov <loginov.vasily@gmail.com>
2016-02-02 16:50:34 +0600

    Moved unzip logic into a separate file.

--- client.js
+++ client.js
@@ -31,58 +31,6 @@ var Compressor = function() {
     };
 };
 
-function convertBlobToUintArray(blob, callback) {
-    var fileReader = new FileReader();
-    fileReader.onload = function(progressEvent) {
-        var arrayBuffer = fileReader.result;
-        var uintArray = new Uint8Array(arrayBuffer);
-        callback(null, uintArray);
-    };
-    fileReader.readAsArrayBuffer(blob);
-}
-
-var Gunzipper = function (gzipFile, chunkSize) {
-    var self = this;
-
-    self.file = gzipFile;
-    self.chunkSize = chunkSize;
-    self.currentPosition = 0;
-    self.decompressor = new pako.Inflate({
-        gzip: true
-    });
-
-    self.decompress = function (numChunks, callback) {
-        var file = self.file;
-        var lastByteNumber = file.size - 1;
-        var currentPosition = self.currentPosition;
-
-        var endPosition = currentPosition + numChunks * self.chunkSize;
-        if (endPosition > lastByteNumber) {
-            endPosition = lastByteNumber;
-        }
-        var isAnythingLeft = endPosition !== lastByteNumber;
-        console.log('Reading chunk from ' + currentPosition + ' to ' + endPosition);
-        console.log('have' + (isAnythingLeft ? '' : ' nothing') + ' more');
-        var chunkBlob = file.slice(currentPosition, endPosition);
-        convertBlobToUintArray(chunkBlob, function (error, chunkArray) {
-            if (self.decompressor.push(chunkArray, pako.Z_SYNC_FLUSH)) {
-                // CAUTION! If set to the endPosition + 1, the CRC check will fail, I don't know why.
-                self.currentPosition = endPosition;
-                callback(null, {
-                    decompressedData: self.decompressor.result,
-                    totalBytesRead: endPosition,
-                    isAnythingLeft: isAnythingLeft
-                });
-                if (!isAnythingLeft) {
-                    self.decompressor.push([], pako.Z_STREAM_END);
-                }
-            } else {
-                callback(new Error('Failed to decompress data.'));
-            }
-        });
-    };
-};
-
 function putFileStringsToOutput(stringArray) {
     var resultHtml = '';
     stringArray.forEach(function (str) {
